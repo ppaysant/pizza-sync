2b191c88e9f4095c3cbe56fdc422180329b9e41f
Maxime Robert <maxime.robert1992@gmail.com>
2017-11-25 18:27:57 +0100

    WIP

--- backend/src/comand-line/command-line.component.ts
+++ backend/src/comand-line/command-line.component.ts
@@ -5,16 +5,22 @@ import { PizzasProvidersModule } from '../features/pizzas-providers/pizzas-provi
 import { ProvidersCommand } from './commands/providers.command';
 import { ProviderCommand } from './commands/provider.command';
 import { Command } from './command.class';
+import { CountdownCommand } from './commands/countdown.command';
+import { OrdersService } from '../features/models/orders/orders.component';
 
 export class CommandLineService {
   private vorpal = _vorpal();
   private commands: Command[] = [];
 
-  constructor(private pizzasProvidersService: PizzasProvidersService) {
+  constructor(
+    private pizzasProvidersService: PizzasProvidersService,
+    private ordersService: OrdersService
+  ) {
     // register all the command by pushing them into `commands` array
     this.commands.push(
       new ProviderCommand(this.vorpal, pizzasProvidersService),
       new ProvidersCommand(this.vorpal, pizzasProvidersService)
+      // new CountdownCommand(this.vorpal, ordersService)
     );
 
     // register every commands
--- backend/src/comand-line/command-line.module.ts
+++ backend/src/comand-line/command-line.module.ts
@@ -3,20 +3,25 @@ import { Module, Inject } from '@nestjs/common';
 import { CommandLineService } from './command-line.component';
 import { PizzasProvidersModule } from '../features/pizzas-providers/pizzas-providers.module';
 import { PizzasProvidersService } from '../features/pizzas-providers/pizzas-providers.component';
+import { OrdersService } from '../features/models/orders/orders.component';
+import { OrdersModule } from '../features/models/orders/orders.module';
 
 // before creating the `CommandLineService` we need to wait
 // for the default pizza provider to be set
 const CommandLineServiceFactory = {
   provide: 'CommandLineService',
-  useFactory: async (pizzasProvidersService: PizzasProvidersService) => {
+  useFactory: async (
+    pizzasProvidersService: PizzasProvidersService,
+    ordersService: OrdersService
+  ) => {
     await pizzasProvidersService.setDefaultProvider();
-    return new CommandLineService(pizzasProvidersService);
+    return new CommandLineService(pizzasProvidersService, ordersService);
   },
-  inject: [PizzasProvidersService],
+  inject: [PizzasProvidersService, OrdersService],
 };
 
 @Module({
-  modules: [PizzasProvidersModule],
+  modules: [PizzasProvidersModule, OrdersModule],
   components: [CommandLineServiceFactory],
 })
 export class CommandLineModule {
